854c85acc87e5a597b39549d6a09bf561f89c8cd
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-06 16:46:06 +0545

    css loader is added

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";console.warn("Other Function","appfile")})();
--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,21 @@ module.exports = {
       path:path.resolve(__dirname, 'dist'),
       filename:'output.js'
   },
+  // loader
+  module: {
+     rules: [
+       {
+         test: /\.css$/,
+         use:[
+           'style-loader',
+           'css-loader'
+         ]
+       }
+     ]
+  },
   devServer: {
     contentBase:path.join(__dirname, 'dist'),
     compress:true,
-    port: 3000,
+    port: 3800,
   }
 }
